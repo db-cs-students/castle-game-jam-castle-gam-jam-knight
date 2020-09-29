31b18bad954bd4550230f5d6bc529eb16f15b5cb
Kaitlyn R <69911469+DBkaitR@users.noreply.github.com>
2020-09-29 10:38:07 -0400

    Update pxt.json, main.ts, main.py

--- main.ts
+++ main.ts
@@ -6,6 +6,8 @@ is to kill
 
  */
 //  On Start 
+info.setLife(3)
+info.setScore(0)
 let knight = sprites.create(img`
     . . . . 3 3 3 3 3 3 . . . . . .
     . . . . 3 . . . . 3 . . . . . .
@@ -122,4 +124,22 @@ scene.setTile(2, img`
     . . . . . . . . . . . . . . . .
     . . . . . . . . . . . . . . . .
     . . . . . . . . . . . . . . . .
+`)
+let bat = sprites.create(img`
+    . . f f f . . . . . . . . f f f
+    . f f c c . . . . . . f c b b c
+    f f c c . . . . . . f c b b c .
+    f c f c . . . . . . f b c c c .
+    f f f c c . c c . f c b b c c .
+    f f c 3 c c 3 c c f b c b b c .
+    f f b 3 b c 3 b c f b c c b c .
+    . c 1 b b b 1 b c b b c c c . .
+    . c 1 b b b 1 b b c c c c . . .
+    c b b b b b b b b b c c . . . .
+    c b 1 f f 1 c b b b b f . . . .
+    f f 1 f f 1 f b b b b f c . . .
+    f f 2 2 2 2 f b b b b f c c . .
+    . f 2 2 2 2 b b b b c f . . . .
+    . . f b b b b b b c f . . . . .
+    . . . f f f f f f f . . . . . .
 `)
