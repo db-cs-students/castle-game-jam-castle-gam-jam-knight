f2722cbab83360f9b24ab6bbb4b50123c289a6e4
Kaitlyn R <69911469+DBkaitR@users.noreply.github.com>
2020-09-30 08:57:05 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -43,6 +43,26 @@ knight.set_position(45, 132)
 controller.move_sprite(knight, 100, 0)
 knight.ay = 100
 scene.camera_follow_sprite(knight)
+bat= sprites.create(img("""
+    . . f f f . . . . . . . . . . .
+    f f f c c . . . . . . . . f f f
+    f f c c c . c c . . . f c b b c
+    f f c 3 c c 3 c c f f b b b c .
+    f f c 3 b c 3 b c f b b c c c .
+    f c b b b b b b c f b c b c c .
+    c c 1 b b b 1 b c b b c b b c .
+    c b b b b b b b b b c c c b c .
+    c b 1 f f 1 c b b c c c c c . .
+    c f 1 f f 1 f b b b b f c . . .
+    f f f f f f f b b b b f c . . .
+    f f 2 2 2 2 f b b b b f c c . .
+    . f 2 2 2 2 2 b b b c f . . . .
+    . . f 2 2 2 b b b c f . . . . .
+    . . . f f f f f f f . . . . . .
+    . . . . . . . . . . . . . . . .
+"""))
+if knight.overlaps_with(bat):
+    info.change_life_by(-1)
 scene.set_tile_map(img("""
     ..................................................
     ..................................................
